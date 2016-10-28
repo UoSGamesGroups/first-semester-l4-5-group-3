9c0726e7921ed9941aa550e0eef34fd778189aa2
Henry Crofts <hwcrofts@gmail.com>
2016-10-28 17:29:34 +0100

    Code snippets
    Added a script for ladders and added the PlayerController script.

---
+++ Development Builds/Code Snippets/LadderZone.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LadderZone : MonoBehaviour
+{
+
+    private PlayerController thePlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)  // Call when player enters ladder "trigger".
+    {
+        if (other.name == "Player")
+        {
+            thePlayer.onLadder = true;  //Changes the players ability to go up and down the ladder.
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)  // Call when player leaves ladder "trigger".
+    {
+        if (other.name == "Player")
+        {
+            thePlayer.onLadder = false;  // Sets player as away from the ladder.
+        }
+    }
+
+
+}
