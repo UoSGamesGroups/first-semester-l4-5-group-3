4611cdca54906145288c87eaf19cf989700e727d
Henry Crofts <hwcrofts@gmail.com>
2016-10-31 15:28:56 +0000

    Uploaded snippets of code
    Code for the checkpoint, kill player (to respawn) and the level manager
    to help with the respawn.

---
+++ Development Builds/Code Snippets/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+    public LevelManager levelManager;  
+
+	// Use this for initialization
+	void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();  // Reference to LevelManager scrip (Change to main script we're going to use).
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")  //  Makes sure object only hurts player.
+        {
+            levelManager.currentCheckpoint = gameObject;  // Causes player to respawn at last checkpoint.
+        }
+    }
+}
---
+++ Development Builds/Code Snippets/KillPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        levelManager = FindObjectOfType<LevelManager>();  // Reference to LevelManager scrip (Change to main script we're going to use).
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")  //  Makes sure to check for Player
+        {
+            levelManager.RespawnPlayer();  // Respawns player
+        }
+    }
+}
