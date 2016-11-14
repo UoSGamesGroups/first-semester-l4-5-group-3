1b706edca6c62502e2ded98b1fc4d1674ae542d6
HeatherBishop <s177801@ucs.ac.uk>
2016-11-14 16:47:24 +0000

    Added GameBuild[14_11_16]

---
+++ GameBuild[14_11_16]/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public GameObject player; //public reference to player
+
+	private Vector3 offset; //public variable to store offset of camera from player.
+
+	// Use this for initialization
+	void Start () 
+	{
+		//find the player game object by searching for the tag "player"
+		player = GameObject.FindGameObjectWithTag ("Player");
+	
+		//offset = distance between players position and the cameras position
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		//set position of cameras transform to = the players + offset
+		transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ GameBuild[14_11_16]/Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ExitLevel : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Check collision name
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+
+
+        }
+    }
+}
