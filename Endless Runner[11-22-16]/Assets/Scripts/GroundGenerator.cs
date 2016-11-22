b83ba367382a13e6032c4fb751ee0eb461b636e3
Henry Crofts <hwcrofts@gmail.com>
2016-11-22 22:42:11 +0000

    Endless Runner Build

---
+++ Endless Runner[11-22-16]/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour
+{
+
+    public GameObject ground;
+    public Transform generator;
+    public float groundDistance;
+
+    private float platformWidth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        platformWidth = ground.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.x < generator.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + groundDistance, transform.position.y, transform.position.z);
+
+            Instantiate(ground, transform.position, transform.rotation);
+        }
+	}
+}
---
+++ Endless Runner[11-22-16]/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public bool grounded = true;
+    
+    private Rigidbody2D rb;
+	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        GetComponent<SpriteRenderer>().flipX = true;
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            grounded = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+    }
+
+
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    //public void Jump()  // Enables you to write Jump(); instead of code below to make code look neater and shorter
+    //{
+    //    rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    //}
+
+}
