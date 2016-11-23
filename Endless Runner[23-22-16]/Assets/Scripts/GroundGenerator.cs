8a8ae8df9ae3b3762a16e2e14bbbc6690227cd87
HeatherBishop <s177801@ucs.ac.uk>
2016-11-23 00:23:04 +0000

    Added endless runner[23-22-16]

---
+++ Endless Runner[23-22-16]/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour
+{
+
+    public GameObject ground;
+    public Transform generator;
+    public float groundDistance;
+
+    private float platformWidth;
+
+
+	//object to spawn items.
+	public GameObject item;
+    public Vector3 itemY = new Vector3(0, 1, 0);
+
+	// Use this for initialization
+	void Start ()
+    {
+        platformWidth = ground.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+        if (transform.position.x < generator.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + groundDistance, transform.position.y, transform.position.z);
+
+            Instantiate(ground, transform.position, transform.rotation);
+
+            Instantiate(item, transform.position + itemY, transform.rotation);
+        }
+	}
+
+}
