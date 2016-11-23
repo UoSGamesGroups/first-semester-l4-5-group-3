38e2664bf702413d7523ff368abd68fb7f3869e5
Henry Crofts <hwcrofts@gmail.com>
2016-11-23 00:02:32 +0000

    A few changes to Endless Runner.
    Kept the old version as wasn't sure if anything had been changed to it.
    Welcome to delete if no changes were done.

---
+++ Endless Runner[11-22-16] v2/Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour
+{
+
+    public GameObject ground;
+    public Transform generator;
+    public float groundDistance;
+
+    private float platformWidth;
+
+    public float groundDistanceMin;
+    public float groundDistanceMax;
+
+    //public ObjectPooling objectPool;
+
+	// Use this for initialization
+	void Start ()
+    {
+        platformWidth = ground.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.x < generator.position.x)
+        {
+            groundDistance = Random.Range(groundDistanceMin, groundDistanceMax);
+
+            transform.position = new Vector3(transform.position.x + platformWidth + groundDistance, transform.position.y, transform.position.z);
+
+            Instantiate(ground, transform.position, transform.rotation);
+            //GameObject newGround = objectPool.GetObjectPool();
+
+            //newGround.transform.position = transform.position;
+            //newGround.transform.rotation = transform.rotation;
+            //newGround.SetActive(true);
+        }
+	}
+}
---
+++ Endless Runner[11-22-16] v2/Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,42 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//public class ObjectPooling : MonoBehaviour
+//{
+//    public GameObject objectPool;
+
+//    public int poolAmount;
+
+//    List<GameObject> pooledObjects;
+
+//	void Start ()
+//    {
+//        pooledObjects = new List<GameObject>();
+
+//        for (int i = 0; i < poolAmount; i++)
+//        {
+//            GameObject obj = (GameObject)Instantiate(objectPool);
+//            obj.SetActive(false);
+//            pooledObjects.Add(obj);
+//        }
+//	}
+
+//    public GameObject GetObjectPool()
+//    {
+//        for (int i = 0; i < pooledObjects.Count; i++)
+//        {
+//            if (pooledObjects[i].activeInHierarchy)
+//            {
+//                return pooledObjects[i];
+//            }
+//        }
+
+//        GameObject obj = (GameObject)Instantiate(objectPool);
+//        obj.SetActive(false);
+//        pooledObjects.Add(obj);
+//        return obj;
+
+//    }
+	
+//}
