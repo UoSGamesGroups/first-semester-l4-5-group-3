8c57378decbf33fecb5b2dd14b89029cd16f8ec0
HeatherBishop <s177801@ucs.ac.uk>
2016-12-07 21:37:01 +0000

    Added endless runner 07_12_16
    Deleted old builds.
    Added meeting minutes.

---
+++ Endless Runner [07-12-16]v2/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Leaderboard : MonoBehaviour {
+	
+	public int highScore;
+	public int currentHighScore;
+	string highScoreKey = "HighScore";
+
+	//sprites of stars to swap as nessisary
+	public SpriteRenderer star1;
+	public SpriteRenderer star2;
+	public SpriteRenderer star3;
+
+	public Sprite starEmpty;
+	public Sprite starFull;
+
+
+
+	void Start(){
+		highScore = PlayerPrefs.GetInt(highScoreKey,0);
+		//use this value in whatever shows the leaderboard.
+
+		star1 = GameObject.Find("star01").GetComponent<SpriteRenderer>();
+		star2 = GameObject.Find("star02").GetComponent<SpriteRenderer>();
+		star3 = GameObject.Find ("star03").GetComponent<SpriteRenderer>();
+		//star2 = GameObject.GetComponent<SpriteRenderer> ().sprite;
+		//star3 = GameObject.GetComponent<SpriteRenderer> ().sprite;
+
+		star1.sprite = starEmpty;
+		star2.sprite = starEmpty;
+		star3.sprite = starEmpty;
+
+		if (highScore > currentHighScore) 
+		{
+			//set current high score to be the new high score.
+			currentHighScore = highScore;
+
+			//add more stars to screen
+			if (currentHighScore >= 3) 
+			{
+				star1.sprite = starFull;
+				Debug.Log ("score is greater than 3");
+
+				if (currentHighScore >= 6)
+				{
+					star2.sprite = starFull;
+					Debug.Log ("Score is > 6");
+
+					if (currentHighScore >= 9) 
+					{
+						star3.sprite = starFull;
+						Debug.Log ("Score is > 9");
+					}
+				}
+			}
+
+		}
+	}
+
+
+	public void StartGameOver()
+	{
+		PlayerPrefs.DeleteAll();
+		SceneManager.LoadScene (0);
+	}
+
+
+
+
+}
---
+++ Endless Runner [07-12-16]v2/Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public int score = 0;
+	public int highScore = 0;
+	string highScoreKey = "HighScore";
+
+	void Start(){
+		//Get the highScore from player prefs if it is there, 0 otherwise.
+		highScore = PlayerPrefs.GetInt(highScoreKey,0);    
+	}
+
+	void Update(){
+		//GetComponent<GUIText>().text = "Score:" + score.ToString();
+	}
+
+	void OnDisable(){
+		         
+		//If our scoree is greter than highscore, set new higscore and save.
+		if(score>highScore){
+			PlayerPrefs.SetInt(highScoreKey, score);
+			PlayerPrefs.Save();
+		         }
+	}
+
+	public void ClearPlayerPref()
+	{
+		PlayerPrefs.SetInt (highScoreKey, 0);
+	}
+
+}
